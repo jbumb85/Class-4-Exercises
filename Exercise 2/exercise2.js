df71547d35dc5639a9dca89063aaca4af5911f5c
jbumb85 <111322252+jbumb85@users.noreply.github.com>
2022-08-25 20:29:13 -0500

    Initial commit

---
+++ Exercise 2/exercise2.js
@@ -0,0 +1,23 @@
+// Exercise #2: Intermediate
+// Aim: Create a function that returns the number of true values in an array.
+
+// Examples
+
+// countTrue([true, false, false, true, false])    ➞    2
+
+// countTrue([false, false, false, false])    ➞    0
+
+// countTrue([])    ➞    0
+// Notes
+
+// Return 0 if given an empty array.
+// All array items are of the type bool (true or false).
+
+
+
+function countValues(group) {
+    for(let i = 0; i < group.length; i++)
+        if(group[i] === "true") {
+
+        }
+    }        
